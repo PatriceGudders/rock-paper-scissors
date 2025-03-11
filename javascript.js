e71604fffd8b41a5be18672349a482d261f8c531
PatriceGudders <gudderspatrice@hotmail.com>
2025-03-11 17:04:08 +0100

    Create function computer choice
    If statement that returns string depending on value of random number.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+let computerChoice;
+
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    console.log(randomNumber);
+    if (randomNumber <= 0.33) {
+        return computerChoice = "Rock";
+    } else if (randomNumber > 0.66) {
+        return computerChoice = "Paper";
+    } else {
+        return computerChoice = "Scissors";
+    }
+     
+}
+
+console.log(getComputerChoice());
